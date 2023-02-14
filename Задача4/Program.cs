e7a5fa3aef80f30326321759e6eb510ef46f56b5
Irina <pimaki2012@mail.ru>
2023-02-14 14:57:33 +0300

    сравнение 2 заданных чисел

---
+++ Задача4/Program.cs
@@ -0,0 +1,18 @@
+﻿// вариант где нужно вводить данные самим
+Console.WriteLine("введите два числа");
+int a, b;
+Console.WriteLine("Введите первое число");
+a = Convert.ToInt32(Console.Read());
+
+Console.WriteLine("Введите второе число");
+b = Convert.ToInt32(Console.Read());
+
+int max = a;
+
+if(b > max) 
+{
+    max = b;
+} 
+
+Console.Write("max = ");
+Console.WriteLine(max);
